60595b2311a47a1ced8b9489b66d0d6e37fe99a3
kali <seipherdj@gmail.com>
2020-11-17 11:21:12 -0600

    add directory flags - comment out os.Getdd() conditional

--- httpserver.go
+++ httpserver.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	//"os"
 )
 
 func main() {
 
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
+	path := flag.String("d", "", "Path  to Directory")
+	flag.Parse()
+	/*if *path == "" {
+		path, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
 	}
-
-	fs := http.FileServer(http.Dir(path))
+	*/
+	fs := http.FileServer(http.Dir(*path))
 
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "text/html")
