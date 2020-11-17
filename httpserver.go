07960217d72b1f50b201ef8631d2b51c6938fd0e
Joey Kavanaugh <seipherdj@iMac.lan>
2020-11-16 18:09:47 -0600

    adding to main

---
+++ httpserver.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+  "fmt"
+)
+
+func main() {
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	fs := http.FileServer(http.Dir(path))
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(res, "<h1>ABGEHTTPTransfer</h1>")
+	})
+
+	http.Handle("/static", fs)
+
+	log.Fatal(http.ListenAndServe(":6969", nil))
+}
